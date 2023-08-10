1e37bd5fe987189afbbd1b67168d7d4860f8a997
slipstream8125 <hatkardhruv@gmail.com>
2023-08-10 23:48:20 +0530

    Finished level2 of day 6

---
+++ day-06/Level 2/question2/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+
+function hexaColor(){
+  let str = '0123456789abcdef'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
+  }
+  return '#' + color
+}
+
+const Numbers=({numbers})=>{
+  const number=numbers.map((numbers) => <div class='box' style={{backgroundColor: hexaColor()}}><p>{hexaColor()}</p></div>)
+  return number
+}
+
+const App=()=>{
+  var numbers=Array();
+  for(var i=0;i<=7;i++){
+    numbers.push(i);
+  }
+  return(
+  <div>
+  <h1> 30 Days of React</h1>
+  <p>Hexadecimal Colors</p>
+    <div class='inline'>
+    <div class='flexbox'>
+      <Numbers numbers={numbers}/>
+    </div>
+    <div class='flexbox'><Numbers numbers={numbers}/></div>
+    <div class='flexbox'><Numbers numbers={numbers}/></div>
+    <div class='flexbox'><Numbers numbers={numbers}/></div>
+    </div>
+    </div>
+  )
+}
+ReactDOM.render(<App/>,rootElement)
