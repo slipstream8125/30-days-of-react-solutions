66239e7f799a26b87689f0206d99e7d66806e25e
slipstream8125 <hatkardhruv@gmail.com>
2023-08-13 18:12:44 +0530

    Finished day 8

---
+++ day-08/level-2/question2/src/index.js
@@ -0,0 +1,229 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import asabenehImage from './images/asabeneh.jpg'
+import {countries}  from  './data/countries.js'
+import ReactCountryFlag from "react-country-flag"
+import lookup from 'country-code-lookup'
+// Fuction to show month date year
+
+const showDate = (time) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+
+  const month = months[time.getMonth()].slice(0, 3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return ` ${month} ${date}, ${year}`
+}
+// User Card Component
+const UserCard = ({count}) => (
+  <div>
+    <div>
+      <object type="image/svg+xml" data={countries[count].flag} width="40" height="60"/>
+      </div>
+  <div className='user-card'>
+    {/* <img src={image} alt={name} /> */}
+
+    <h4>{countries[count].name}</h4>
+     <p class="cclass"><b>Capital :</b> {countries[count].capital}</p> 
+     <p class="cclass"><b>Languages :</b> {countries[count].languages.join(",")}</p>
+     <p class="cclass"><b>Population :</b> {countries[count].population.toLocaleString()}</p>
+     <p class="cclass"><b>Currency :</b> {countries[count].currency}</p>
+  </div>
+  </div>
+)
+const Count = ({ count, addOne}) => (
+  <div>
+    <div>
+      <Button text='Select Country' onClick={addOne} style={buttonStyles} />
+    </div>
+  </div>
+)
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+console.log(countries[0].flag);
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+// class based component
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header style={this.props.styles}>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+// TechList Component
+// class base component
+
+// Main Component
+// Class Component
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {
+      techs,
+      user,
+      greetPeople,
+      handleTime,
+      changeBackground,
+      count,
+      addOne,
+      minusOne,
+    } = this.props
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <ul>
+            {/* <TechList techs={techs} /> */}
+          </ul>
+          <UserCard count={count}/>
+
+          <Count count={count} addOne={addOne}/>
+        </div>
+      </main>
+    )
+  }
+}
+
+// Footer Component
+// Class component
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    count: 0,
+    styles: {
+      backgroundColor: '',
+      color: '',
+    },
+  }
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  addOne = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  changeBackground = () => {}
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const date = new Date()
+    // copying the author from data object to user variable using spread operator
+
+    return (
+      <div className='app'>
+        {this.state.backgroundColor}
+        <Header data={data} />
+        <Main
+          addOne={this.addOne}
+          minusOne={this.minusOne}
+          count={this.state.count}
+        />
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
