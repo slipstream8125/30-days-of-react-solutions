1945097c7f146a32c37234b39b43224adc719174
slipstream8125 <hatkardhruv@gmail.com>
2023-08-18 20:19:08 +0530

    Finished day 18 level 2

---
+++ day-18/level2/src/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const Country = ({ country: { name, life_span},length }) => {
+  var totalalife=0;
+  // for(var i=0;i<length;i++){
+   
+  
+  // totalalife/=length
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        {/* <img src={flag} alt={name} /> */}
+      </div>
+      <h3 className='country_name'>{length+" "+name +" "+(totalalife)}</h3>
+      <div class='country_text'>
+        {/* <p>
+          <span>Population: </span>
+          {population} */}
+        {/* </p> */}
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  state = {
+    data: [],
+    avglife: 0,
+  }
+
+  componentDidMount() {
+    this.fetchCountryData()
+    
+  }
+  fetchCountryData = async () => {
+    const url = 'https://api.thecatapi.com/v1/breeds'
+    try {
+      const response = await axios.get(url)
+      const data = await response.data
+      this.setState({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getlifespandata(){
+    var lifespan;
+    var alife=0;
+    var avglif=0.00;
+    for(var i=0;i<this.state.data.length;i++){
+      lifespan=this.state.data[i].life_span.split(" - ");
+      alife=(parseInt(lifespan[1])+parseInt(lifespan[0]))/2
+      avglif+=alife;
+    }
+    avglif=Math.round((avglif/this.state.data.length)*100)/100;
+    // console.log(avglif/this.state.data.length);
+    // console.log(this.state.data);
+    return parseFloat(String(avglif),2);
+  }
+  getWeight(){
+    var wgt;
+    var awgt=0;
+    var avgwgt=0.00;
+    for(var i=0;i<this.state.data.length;i++){
+      wgt=this.state.data[i].weight.metric.split(" - ");
+      awgt=(parseInt(wgt[1])+parseInt(wgt[0]))/2
+      avgwgt+=awgt;
+    }
+    avgwgt=Math.round((avgwgt/this.state.data.length)*100)/100;
+    // console.log(avglif/this.state.data.length);
+    // console.log(this.state.data);
+    return parseFloat(String(avgwgt),2);
+  }
+  render() {
+    // this.getlifespandata()
+    return (
+      <div className='App'>
+        <h1>30 Days of React</h1>
+        <h2>Cats Paradise</h2>
+        <div>
+          <h3>There are <span class='dot'>{this.state.data.length}</span> cats in the api</h3>
+          {/* <div className='countries-wrapper'>
+            {this.state.data.map((country) => (
+              <Country country={country} length={this.state.data.length}/>
+            ))}
+          </div> */}
+          {/* <button >Life Span</button> */}
+          <p>On average a cat can weight about <span class="dot">{this.getWeight()}</span>Kg and live <span class="dot">{this.getlifespandata()}</span>years</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
