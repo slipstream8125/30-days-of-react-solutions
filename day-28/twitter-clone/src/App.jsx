f5ec52e5401dbab0aa7f351326eb5da9efaf2580
slipstream8125 <hatkardhruv@gmail.com>
2023-08-29 20:59:05 +0530

    Finished Day 28

--- day-28/twitter-clone/src/App.jsx
+++ day-28/twitter-clone/src/App.jsx
@@ -8,9 +8,10 @@ import { TextField,Card,Box,Button} from '@mui/material'
 function App() {
   const [todoList,setTodoList] = useState([]);
   const [newTask,setNewTask]=useState("");
-
+  const [rem,setnewRem]=useState(250);
   const handleChange=(event)=>{
     setNewTask(event.target.value);
+    setnewRem(250-event.target.value.length);
   };
   const addTask=()=>{
     const task = {
@@ -33,8 +34,13 @@ function App() {
   return (
       <div className="App">
         <div className='addTask'>
-        <TextField id="outlined-basic" sx={{width : '50%'}} onChange={handleChange}/>{' '}
-          <Button  size="lg"  variant='contained' onClick={addTask} sx={{padding: '1em'} }>Add Task</Button>
+          <Card variant='outlined'>
+            <div style={{padding: '2em'}}>
+        <TextField id="outlined-basic" sx={{width : '50%',paddingTop:'2%'}} onChange={handleChange} placeholder='Tweet Something'/>{' '}
+          <Button  size="lg"  variant='contained' onClick={addTask} sx={{padding: '1em',backgroundColor: 'DodgerBlue',borderRadius: '30px',marginTop:'2%',marginLeft:'20px'} }>Add Tweet</Button>
+          <p align='left'style={{marginLeft:'67%'}}>{rem}</p>
+          </div>
+          </Card>
         </div>
         <div>
         <Box>
--- day-28/twitter-clone/src/components/Task.jsx
+++ day-28/twitter-clone/src/components/Task.jsx
@@ -1,9 +1,41 @@
-import {Button} from '@mui/joy'
-import DeleteIcon from '@mui/icons-material/Delete';
-export const Task= (props)=>(
-    <div className='task'>
-        <h1>{props.taskName}</h1>
-        <Button onClick={() => props.deleteTask(props.id)}><DeleteIcon/></Button>
-    </div>
-)
+import { IconButton ,ButtonGroup, Icon,Button} from '@mui/material';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import {FaRegComment,FaRegHeart,FaRetweet,FaCircleUser} from "react-icons/fa6"
+import {FaRegEdit,FaUserCircle} from 'react-icons/fa'
+import '../App.css'
 
+
+export const Task= (props)=>{
+    const curDate=new Date();
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const date=`${monthNames[curDate.getMonth()]} ${curDate.getDate()}, ${curDate.getFullYear()} ${curDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
+ return(
+    <div style={{display: 'block'}}>
+        <div class='avatar' style={{alignItems:'left',justifyContent: 'left',textAlign: 'left',display : 'flex'}}>
+            <IconButton sx={{float: 'left' }}><FaUserCircle size="1.5em"/></IconButton>
+            <h1 style={{fontSize: '1.4em',fontWeight:'500',paddingTop: '10px'}}>Dhruv Hatkar</h1>
+            <span style={{paddingTop: '15px',paddingLeft:'5px'}}>@Slipstream</span>
+            <br/>
+        </div>
+        <div >
+        <h1 style={{marginTop: '10px',fontSize: '1.2em',textAlign:'left',paddingLeft: '10px'}}>{props.taskName}</h1>
+        </div>
+        <div className='task' style={{justifyContent: 'space-between'}}>
+            <div className='buttongroup1'><ButtonGroup spacing="0.5rem" aria-label="spacing button group" style={{marginRight: '40%'}}>
+             <IconButton color='primary'style={{boxShadow: "none"}} elevation={0} disableElevation={true}><FaRegEdit/></IconButton>
+             <IconButton  color='primary'style={{boxShadow: "none"}} elevation={0} onHover={{color:'red'}} onClick={() => props.deleteTask(props.id)} ><DeleteOutlinedIcon/></IconButton>
+            </ButtonGroup>
+            </div>
+            <div><ButtonGroup spacing="0.5rem" aria-label="spacing button group"style={{position: 'relative',float:'left'}}>
+                <IconButton><FaRegComment/></IconButton>
+                <IconButton ><FaRegHeart/></IconButton>
+                <IconButton><FaRetweet/></IconButton>
+                </ButtonGroup>
+            </div>
+            <div style={{marginRight: '5px'}}>
+                <p>{date}</p>
+            </div>
+        </div>
+    </div>
+    )
+}
